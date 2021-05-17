ad6e00ada3741bf9385296c1d9cbf95158f97782
Adriano Zimmermann <53878585+adrianorsl@users.noreply.github.com>
2021-05-17 19:26:42 -0300

    Add files via upload

--- MVC/src/Enum/Tipos.java
+++ MVC/src/Enum/Tipos.java
@@ -6,23 +6,17 @@ import com.classes.AlunoBO;
 
 public enum Tipos  {
 
-	JSON ("arquivos/Alunos.json", new Json()), 
-	XML ("arquivos/Alunos.xml", new XML()),
-	BD ("Aluno", new AlunoBO());
+	JSON (new Json()), 
+	XML (new XML()),
+	BD (new AlunoBO());
 	
-	private  String caminho;
+	
 	private Persistencia persistencia;
 	
-	private Tipos(String caminho, Persistencia persistencia) {
-		this.caminho = caminho;
+	private Tipos(Persistencia persistencia) {
 		this.persistencia = persistencia;
 	}
 
-	
-
-	public String getCaminho() {
-		return caminho;
-	}
 
 	public Persistencia getPersistencia() {
 		return persistencia;
